fea52532d82c808e69007ab27b505112182b6e25
lenovo <lenovo@Lenovo-LISHIPENG>
2019-01-19 09:41:12 +0800

    lsp

--- src/main/java/cn/bocom/r_entity/process/proc/SplitProc.java
+++ src/main/java/cn/bocom/r_entity/process/proc/SplitProc.java
@@ -2,5 +2,20 @@ package cn.bocom.r_entity.process.proc;
 
 public class SplitProc{
 
-
+	private String symbol;
+	private String index;
+	
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public String getIndex() {
+		return index;
+	}
+	public void setIndex(String index) {
+		this.index = index;
+	}
+	
 }
